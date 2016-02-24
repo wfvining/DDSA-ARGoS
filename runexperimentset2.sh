ed58af56d0064e139478f7ddf29a2646379282eb
Matthew Fricke <matthew@fricke.co.uk>
2016-02-23 21:59:35 -0700

    Bug fix: wrong output path in experiment scripts

--- runexperimentset2.sh
+++ runexperimentset2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 0 1 2 3 4 5 6 7 8 9 10
 do
-    ./runntimes.sh 25 experiments/DDSAPLError${i}.xml final_results/DDSAPLError${i}.txt
+    ./runntimes.sh 25 experiments/DDSAPLError${i}.xml results/DDSAPLError${i}.txt
 done
