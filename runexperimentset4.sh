468b059ec577b083b7f8b9c6bc79d0c9fe691dc3
Matthew Fricke <matthew@philautia.cs.unm.edu>
2016-02-23 22:30:17 -0700

    Fixed experiment script bugs

--- runexperimentset4.sh
+++ runexperimentset4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 10 12 14 16 18 20
 do
-    ./runntimes.sh $1 experiments/DDSAU60minSize${i}x${i}.xml results/DDSAU60minSize${i}x${i}.txt
+    ./runntimes.sh $1 experiments/DDSAU60minArea${i}x${i}.xml results/DDSAU60minArea${i}x${i}.txt
 done
